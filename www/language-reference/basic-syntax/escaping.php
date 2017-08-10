a96b5753a75d915809657b0bb5383d15c881fda6
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-08-10 16:49:26 +0300

    [feature] html escaping

--- www/language-reference/basic-syntax/escaping.php
+++ www/language-reference/basic-syntax/escaping.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksandrnovikov
- * Date: 10.08.17
- * Time: 12:57
- */
+
+?>
+<p>PHP code is separated from plain text. This text is ignored by interpretator and outputs as-is</p>
+<?= 'This text is processed' ?>
+<?php if(true): ?>
+<p>this text is outputted</p>
+<?php else: ?>
+    this text is never reached
+<?php endif; ?>
+<p>Plain text is more effective then echo or print();</p>
+<h4>starting from PHP5.4 always avaliable</h4>
+<ul>
+    <li>&lt;?php ?></li>
+    <li>&lt;= ?> (before 5.4 needed short open tag option on)</li>
+    <li>&lt;script language="php"> &lt;/script></li>
+</ul>
+<ul>
+    <li>&lt;? ?> enabled with short_open_tags option on in php.ini</li>
+    <li>&lt;% %>  &lt;%= %> available with asp_tags=true in php.ini</li>
+</ul>
+
+<h4>starting from PHP7 avaliable</h4>
+<ul>
+    <li>&lt;php ?></li>
+    <li>&lt;= ?></li>
+    <li>&lt;? ?> enabled with short_open_tags option on in php.ini (not recommended)</li>
+</ul>
--- www/language-reference/basic-syntax/phptags.php
+++ www/language-reference/basic-syntax/phptags.php
@@ -3,7 +3,11 @@
 ?>
 
 <?
-    echo "Short tags";
+    echo "Short tags<br>";
+?>
+
+<?
+echo "Short tags cause error if short_open_tag in php.ini is set to false";
 ?>
 
 <p>Not available anymore starting from PHP7.0:</p>
