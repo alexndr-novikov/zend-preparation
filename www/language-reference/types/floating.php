d7af625c6d080fb5f76542ddd30b54fa65d91a61
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-08-14 15:51:15 +0300

    [feature] covered strings and floats

--- www/language-reference/types/floating.php
+++ www/language-reference/types/floating.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksandrnovikov
- * Date: 11.08.17
- * Time: 14:12
- */
+error_reporting(E_ALL);
+
+$a = 1.234;
+$b = 1.1e2; // 1.1 * 10^2
+$c = 7e-1; // 7 + 10^-1
+
+var_dump($a, $b, $c);
+
+echo '<h3>Converting</h3>';
+
+var_dump((float)((object)[0 => 'a'])); // generates warning
+
+var_dump((int)((0.7+0.1) * 10)); // outputs 7, http://floating-point-gui.de/basic/
+
+
+echo '<h3>Compare</h3>';
+
+$a = 1.123456789;
+$b = 1.123456780;
+$epsilon = 0.00001;
+
+var_dump($a === $b); // false
+var_dump(abs($a - $b)); // 9.0000000785295E-9
+var_dump(abs($a - $b) * 1e9); // 9.0000000785295 , not 9
+var_dump(abs($a - $b) < $epsilon); // true
+echo "<br>NaN:<br>";
+// NaN compared to anything exept true is false (even to itself)
+echo "NAN == NAN: " ; var_dump(NAN == NAN); echo '<br>'; // false
+echo "NAN !== true: " ; var_dump(NAN !== true); echo '<br>'; // true
+echo "is_nan(NAN): " ; var_dump(is_nan(NAN)); echo '<br>'; // true
+
--- www/language-reference/types/strings.php
+++ www/language-reference/types/strings.php
@@ -1,7 +1,109 @@
+<h1>Strings</h1>
 <?php
-/**
- * Created by PhpStorm.
- * User: aleksandrnovikov
- * Date: 11.08.17
- * Time: 14:12
- */
+
+$str1 = 'Single quotes, outputs \ as slash, \\ as slash; Can output \' either; Variables $are not handled <br>';
+echo $str1;
+$str2 = "
+\r - caret return
+\n - new line
+\t - horizontal tab
+\v - vertical tab
+\e - escape symbol
+\\ - backslash
+\$ - dollar sign
+\" - double quote
+\f - form feed<br>
+also variables handling is possible
+";
+echo $str2;
+?>
+<h2>Heredoc</h2>
+<?php
+$variable = 'pasted value';
+$heredoc = <<<HANDLED
+    Here is text
+     <p><strong>{$variable}</strong> - variables are handled</p>
+        
+HANDLED;
+
+$nowdoc = <<<'NOT_HANDLED'
+    Here is text
+     <p><strong>{$str}</strong> - variables are not handled</p>
+        
+NOT_HANDLED;
+
+echo $heredoc, $nowdoc;
+
+class Nowdoc
+{
+    public static $var = <<<'TEXT'
+    test
+TEXT;
+
+}
+
+echo Nowdoc::$var;
+?>
+
+<h3>Variables processing</h3>
+<h4>Simple syntax</h4>
+<?php
+$var = 'simple var';
+$array = ['array_field' => 'Array field'];
+$object = (object)['field' => 'Object field'];
+
+echo "Simple var : $var<br> Array field: $array[array_field]<br> Object field: $object->field<br>";
+
+?>
+<h4>Complex syntax</h4>
+<?php
+$arr = [];
+$arr[] = ['some' => 'string'];
+?>
+<?=
+ "{$arr[0]['some']}";
+?>
+
+
+<?php
+class StaticValues
+{
+    public static $str = 'static_field';
+    const constant = 'constant';
+}
+$static_field = '1';
+$constant = '2';
+echo "{${StaticValues::$str}} , {${StaticValues::constant}}";
+?>
+
+<h4>Index access</h4>
+<?php
+$string = 'Something';
+echo $string[3]; // e
+echo $string{3}; // e
+echo $string[-1]; // g , starting from PHP 7.1
+echo $string{-1}; // g , starting from PHP 7.1
+$a = 'str';
+$a[5] = 'genius';
+echo $a; // str  g, adds spaces where indexes didn't exist, assigns first char of the string;
+$a[7] = ''; // warning - forbidden to add empty string
+?>
+<h4>Variables to strings</h4>
+<p><?= 1; ?>  - 1</p>
+<p><?= 1.23; ?>  - 1.23. . or , defined by locale</p>
+<p><?= true; ?>  - 1</p>
+<p><?= false; ?>  - empty string</p>
+<p><?= []; ?>  - Array</p>
+<?php
+class Stringable
+{
+    function __toString()
+    {
+        return '__toString() method output';
+    }
+
+}
+?>
+<p><?= new Stringable(); ?>  - Object</p>
+<p><?= curl_init(); ?>  - Resource id #2</p>
+
