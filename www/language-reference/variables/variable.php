798cd364dd548d60916b9ff6e52a5f7588198fbe
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-08-20 23:01:45 +0300

    variable variables

--- www/language-reference/variables/variable.php
+++ www/language-reference/variables/variable.php
@@ -1,1 +1,35 @@
 <?php
+
+
+$variable = 'Hello';
+
+$Hello = 'my';
+
+$my = 'name';
+
+$name = 'is';
+
+$is = 'Alex';
+
+echo "$variable {$$variable} {$$$variable} {$$$$variable} {$$$$$variable}"; // Hello my name is Alex
+
+class Test
+{
+    public $variable = 'Hello';
+
+    public $Hello = 'my';
+
+    public $my = 'name';
+
+    public $name = 'is';
+
+    public $is = 'Alex';
+}
+
+$object = new Test();
+echo "<br>$object->variable 
+    {$object->{$object->variable}} 
+    {$object->{$object->{$object->variable}}}
+    {$object->{$object->{$object->{$object->variable}}}}
+    {$object->{$object->{$object->{$object->{$object->variable}}}}}
+    "; // Hello my name is Alex
