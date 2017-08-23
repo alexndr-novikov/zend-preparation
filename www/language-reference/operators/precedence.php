8a3734f794e23d903b5187e7be2fbcbf51bbbb63
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-08-23 22:20:06 +0300

    [feature] operatores precedence

--- www/language-reference/operators/precedence.php
+++ www/language-reference/operators/precedence.php
@@ -1,1 +1,16 @@
 <?php
+
+$b = 1;
+$a = $b += 3;
+
+var_dump($a, $b); // b = 4, a = 4
+
+$a = true ? false : true ? 2 : 3;
+
+var_dump($a); // 3
+
+$a = 1;
+$b = 1 + $a++;
+$c = 1 + ++$a;
+
+var_dump($b, $c); // 2, 4
