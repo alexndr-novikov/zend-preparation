2d994af8701bfd98f00f5c4bf437c44f8ecd2940
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-08-30 22:07:36 +0300

    [feature] logical operators

---
+++ www/language-reference/operators/logical.php
@@ -0,0 +1,27 @@
+<h1>Logical Operators</h1><?php
+$a = false || true; // $a = (false || true);
+$b = false or true; // (($b = false) or true);
+
+var_dump($a, $b, $b = false or true);
+echo "<br><br>";
+
+
+$a = true && false; // $a = (true && false);
+$b = true and false; // (($b = true) and false);
+
+var_dump($a, $b, $b = true and false);
+echo "<br><br>";
+
+
+function foo() : bool
+{
+    echo 'Never echo please';
+    return true;
+}
+
+$a = true || foo(); // no foo() call
+$b = true or foo(); // no foo() call
+$c = false && foo(); // no foo() call
+$d = false and foo(); // no foo() call
+
+var_dump($a, $b, $c, $d);
