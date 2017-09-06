f5576f1604b12f49f7503b40babb71871f72fb05
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-09-06 15:06:03 +0300

    [feature] added oop map, updated hosts file - static css / js files served by nginx now. Added bootstrap locally + common styles

--- www/src/models/SubCategory.php
+++ www/src/models/SubCategory.php
@@ -23,7 +23,7 @@ class SubCategory
     public function render($parentDir) : string
     {
 
-        return "<h4 data-toggle='collapse' data-target='#{$this->id}' aria-expanded='false' aria-controls='collapseExample'>$this->name</h4>"  . $this->renderChildren($parentDir . '/' . $this->directory);
+        return "<h4 class='collapse-control' data-toggle='collapse' data-target='#{$this->id}' aria-expanded='false' aria-controls='collapseExample'>$this->name</h4>"  . $this->renderChildren($parentDir . '/' . $this->directory);
     }
 
     private function renderChildren($directory)
