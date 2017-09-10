13dec78edb38b96d0e7864a5f4c8a63758e5f083
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-09-10 20:45:52 +0300

    [feature] object comparison

---
+++ www/language-reference/oop5/object-comparison.php
@@ -0,0 +1,36 @@
+<h1>Comparing Objects</h1><?php
+
+
+class Comparable
+{
+    private $prop1;
+
+    public function __construct($value)
+    {
+        $this->prop1 = $value;
+    }
+}
+
+class ComparableExtended extends Comparable { }
+
+$o = new Comparable(true);
+$same = new ComparableExtended(true);
+$b = new Comparable(1);
+$o2 = $o;
+$o3 = clone $o;
+br();
+var_dump($o == $b); // true
+var_dump($o === $b); // false
+br();
+var_dump($o == $same); // false
+var_dump($o === $same); // false
+br();
+var_dump($o == $o2); // true
+var_dump($o === $o2); // true
+br();
+var_dump($o == $o3); // true
+var_dump($o === $o3); // false
+
+
+// == equals to each property of obj1 == each prop of obj2 && obj1 inctanceof same class
+// === equals to reference on same obj
