2e0656ef5b20b644dc07e3c9d39c53136d378ad1
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-09-12 22:26:40 +0300

    [feature] anonymous class

---
+++ www/language-reference/oop5/anonymous.php
@@ -0,0 +1,65 @@
+<h1>Anonymous classes</h1><?php
+
+interface Handable
+{
+    public function handle();
+}
+
+trait handlerTrait
+{
+    public $prop = 'handled';
+
+    public function handle()
+    {
+        echo $this->prop;
+    }
+}
+
+class ObjectHandler
+{
+    private static $object;
+
+    public static function handleInner()
+    {
+        self::$object->handle();
+    }
+
+    public static function setClass(Handable $object)
+    {
+        self::$object = $object;
+    }
+}
+
+
+ObjectHandler::setClass(new class('New handle echo') implements Handable
+{
+    use handlerTrait;
+
+    public function __construct($prop)
+    {
+        $this->prop = $prop;
+    }
+
+});
+
+ObjectHandler::handleInner();
+
+function get_anon_class()
+{
+    return new class('New handle echo') implements Handable
+    {
+        use handlerTrait;
+
+        public function __construct($prop)
+        {
+            $this->prop = $prop;
+        }
+
+    };
+}
+br();
+var_dump(get_class(get_anon_class()) === get_class(get_anon_class())); // true
+br();
+echo get_class(get_anon_class());
+br();
+echo get_class(get_anon_class());
