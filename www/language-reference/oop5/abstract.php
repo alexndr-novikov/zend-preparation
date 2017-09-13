c66662fa1f6e0fcc69041e02407012a4b8548a76
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-09-13 23:25:05 +0300

    [feature] abstract classes

---
+++ www/language-reference/oop5/abstract.php
@@ -0,0 +1,30 @@
+<h1>Class Abstraction</h1><?php
+
+set_error_handler(function (...$args) {
+    var_dump($args);
+    return true;
+});
+
+abstract class AbstractClass
+{
+    protected abstract function func($a, $b);
+
+    public function echoStr()
+    {
+        echo 'string';
+        br();
+    }
+}
+
+class AbstractExtender extends AbstractClass
+{
+    public function func // can be public or protected
+    ($a, $b, $d = 20) // allows not required params over abstract one
+    {
+        echo 'smth';
+    }
+}
+
+// var_dump(new AbstractClass);  // will trigger an error
+
+var_dump((new AbstractExtender)->echoStr());
