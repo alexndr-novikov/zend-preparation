8e8916ec8ef79fe136fddc3b65ef5fe6907f46e8
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-10-03 22:27:56 +0300

    [feature] IteratorAggregate

---
+++ www/language-reference/reserved.interfaces/iteratoraggregate.php
@@ -0,0 +1,39 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('IteratorAggregate');
+
+class Agregator implements IteratorAggregate
+{
+    public $prop1 = 'prop1';
+    public $prop2 = 'prop2';
+    public $prop3 = 'prop3';
+
+    public function __construct()
+    {
+        $this->prop4 = 'prop4';
+    }
+
+    public function getIterator()
+    {
+        return new ArrayIterator($this);
+    }
+}
+
+class Agregator2 implements IteratorAggregate
+{
+    public function getIterator()
+    {
+        return (function () {
+            yield from range(1, 10);
+        })();
+    }
+}
+
+foreach ((new Agregator) as $key => $value) {
+    echo sprintf('Key %s, value %s', $key, $value);
+    HTML::br();
+}
+
+foreach ((new Agregator2) as $key => $value) {
+    echo sprintf('Key %s, value %s', $key, $value);
+    HTML::br();
+}
