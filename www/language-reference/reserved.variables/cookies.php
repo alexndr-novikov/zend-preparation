3c3e194f377a0152e95a3ce7c35c1e0faba57f92
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-10-07 20:48:22 +0300

    [feature] $_COOKIE

---
+++ www/language-reference/reserved.variables/cookies.php
@@ -0,0 +1,8 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('$_​COOKIE');
+
+$nextCookieValue = rand(1, 100);
+HTML::tag('p', 'Next cookie value will be: ' . $nextCookieValue);
+setcookie('$_COOKIE', $nextCookieValue);
+
+HTML::tag('pre', print_r($_COOKIE, true));
