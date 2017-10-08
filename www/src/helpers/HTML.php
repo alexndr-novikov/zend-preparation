32d01aed4115f5bc9e98ffa9a0c3d7d3421ce0b4
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-10-08 19:41:42 +0300

    [maintenance] updated HTML class

--- www/src/helpers/HTML.php
+++ www/src/helpers/HTML.php
@@ -5,13 +5,81 @@ namespace Doc\helpers;
 /**
  * Class HTML
  * @package Doc\helpers
- * @method static p($tagValue, $printMode = true)
+ *
+ * @method static abbr($tagValue, $printMode = true)
+ * @method static acronym($tagValue, $printMode = true)
+ * @method static address($tagValue, $printMode = true)
+ * @method static applet($tagValue, $printMode = true)
+ * @method static area($tagValue, $printMode = true)
+ * @method static base($tagValue, $printMode = true)
+ * @method static basefont($tagValue, $printMode = true)
+ * @method static big($tagValue, $printMode = true)
+ * @method static blink($tagValue, $printMode = true)
+ * @method static blockquote($tagValue, $printMode = true)
+ * @method static body($tagValue, $printMode = true)
+ * @method static b($tagValue, $printMode = true)
+ * @method static button($tagValue, $printMode = true)
+ * @method static caption($tagValue, $printMode = true)
+ * @method static center($tagValue, $printMode = true)
+ * @method static cite($tagValue, $printMode = true)
+ * @method static code($tagValue, $printMode = true)
+ * @method static col($tagValue, $printMode = true)
+ * @method static dfn($tagValue, $printMode = true)
+ * @method static dir($tagValue, $printMode = true)
+ * @method static div($tagValue, $printMode = true)
+ * @method static dl($tagValue, $printMode = true)
+ * @method static dt($tagValue, $printMode = true)
+ * @method static dd($tagValue, $printMode = true)
+ * @method static em($tagValue, $printMode = true)
+ * @method static font($tagValue, $printMode = true)
+ * @method static form($tagValue, $printMode = true)
  * @method static h1($tagValue, $printMode = true)
  * @method static h2($tagValue, $printMode = true)
  * @method static h3($tagValue, $printMode = true)
  * @method static h4($tagValue, $printMode = true)
  * @method static h5($tagValue, $printMode = true)
  * @method static h6($tagValue, $printMode = true)
+ * @method static head($tagValue, $printMode = true)
+ * @method static hr($tagValue, $printMode = true)
+ * @method static html($tagValue, $printMode = true)
+ * @method static isindex($tagValue, $printMode = true)
+ * @method static i($tagValue, $printMode = true)
+ * @method static kbd($tagValue, $printMode = true)
+ * @method static link($tagValue, $printMode = true)
+ * @method static li($tagValue, $printMode = true)
+ * @method static map($tagValue, $printMode = true)
+ * @method static marquee($tagValue, $printMode = true)
+ * @method static menu($tagValue, $printMode = true)
+ * @method static meta($tagValue, $printMode = true)
+ * @method static ol($tagValue, $printMode = true)
+ * @method static option($tagValue, $printMode = true)
+ * @method static param($tagValue, $printMode = true)
+ * @method static pre($tagValue, $printMode = true)
+ * @method static p($tagValue, $printMode = true)
+ * @method static q($tagValue, $printMode = true)
+ * @method static samp($tagValue, $printMode = true)
+ * @method static script($tagValue, $printMode = true)
+ * @method static select($tagValue, $printMode = true)
+ * @method static small($tagValue, $printMode = true)
+ * @method static span($tagValue, $printMode = true)
+ * @method static strikeout($tagValue, $printMode = true)
+ * @method static strong($tagValue, $printMode = true)
+ * @method static style($tagValue, $printMode = true)
+ * @method static sub($tagValue, $printMode = true)
+ * @method static sup($tagValue, $printMode = true)
+ * @method static table($tagValue, $printMode = true)
+ * @method static td($tagValue, $printMode = true)
+ * @method static textarea($tagValue, $printMode = true)
+ * @method static th($tagValue, $printMode = true)
+ * @method static tbody($tagValue, $printMode = true)
+ * @method static thead($tagValue, $printMode = true)
+ * @method static tfoot($tagValue, $printMode = true)
+ * @method static title($tagValue, $printMode = true)
+ * @method static tr($tagValue, $printMode = true)
+ * @method static tt($tagValue, $printMode = true)
+ * @method static ul($tagValue, $printMode = true)
+ * @method static u($tagValue, $printMode = true)
+ * @method static va($tagValue, $printMode = true)
  */
 abstract class HTML
 {
@@ -55,8 +123,6 @@ abstract class HTML
         'head',
         'hr',
         'html',
-        'img ',
-        'input',
         'isindex',
         'i',
         'kbd',
@@ -94,33 +160,53 @@ abstract class HTML
         'tt',
         'ul',
         'u',
-        'va>'
+        'va'
     ];
 
-    public static function br()
+    /**
+     * Print/return <br> tag.
+     * @param bool $printMode
+     * @return mixed
+     */
+    public static function br($printMode = true)
     {
-        echo "<br>";
+        return self::handlePrint('br', $printMode);
     }
 
-    public static function a(string $link, string $content)
+    /**
+     * Print/return <a></a> tag.
+     * @param string $link
+     * @param string $content
+     * @param bool $printMode
+     * @return mixed
+     */
+    public static function a(string $link, string $content, $printMode = true)
     {
-        echo "<a href='{$link}'>{$content}</a>";
+        return self::handlePrint("<a href='{$link}'>{$content}</a>", $printMode);
     }
 
+    /**
+     * Print/return pair tag.
+     * @param string $tagName
+     * @param string $tagValue
+     * @param bool $printMode
+     * @return mixed
+     */
     public static function tag(string $tagName, string $tagValue, $printMode = true)
     {
         self::checkWhiteList($tagName);
 
         $tag = "<{$tagName}>$tagValue</{$tagName}>";
 
-        if ($printMode) {
-            echo $tag;
-        }
-
-        return $tag;
+        return self::handlePrint($tag, $printMode);
 
     }
 
+    /**
+     * Check if tag is pair and is white list
+     * @param string $tag
+     * @throws \Exception
+     */
     private static function checkWhiteList(string $tag): void
     {
         if (!in_array($tag, self::TAG_WHITE_LIST)) {
@@ -128,11 +214,32 @@ abstract class HTML
         }
     }
 
+    /**
+     * Let call any pair html tag
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
     public static function __callStatic($name, $arguments)
     {
         list($value, $printMode) = $arguments;
         $printMode = is_null($printMode) ? true : (bool)$printMode;
         return self::tag($name, $value, (bool)$printMode);
+    }
+
+    /**
+     * Return and print tag if necessary
+     * @param $value
+     * @param $printMode
+     * @return mixed
+     */
+    private static function handlePrint($value, $printMode)
+    {
+        if ($printMode) {
+            echo $value;
+        }
+
+        return $value;
     }
 
 
