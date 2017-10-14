0b5697205ba99e4f207b0f63e2d0cce20223bba1
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-10-14 18:39:15 +0300

    [feature] context params

---
+++ www/language-reference/context/params.php
@@ -0,0 +1,27 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('Context parameters');
+
+$postData = http_build_query(
+    array(
+        'author_name' => '1',
+    )
+);
+
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'content' => $postData
+    )
+);
+
+
+
+
+
+$context = stream_context_create();
+
+stream_context_set_params($context, $opts);
+
+$response = file_get_contents('http://demo.wp-api.org/wp-json/wp/v2/comments/1', false, $context);
+
+var_dump($response);
