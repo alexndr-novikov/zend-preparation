68855a915b0121664fdba07b1a30ba700d00974b
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-11-10 21:19:58 +0300

    [feature] shuffle()

---
+++ www/functions/array/shuffle.php
@@ -0,0 +1,11 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('shuffle()');
+
+$array = [10 => 9, 8, 6, 5, 'qwerty' => 4, 3, 32, 1];
+shuffle($array);
+HTML::pre($array);
+shuffle($array);
+HTML::pre($array);
+
+$notArray = '';
+var_dump(shuffle($notArray)); // Warning + false
