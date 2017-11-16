6f0271a3ab4d05420a0bebf3c919bc8c3f4d862c
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-11-16 21:44:41 +0200

    [feature] reset()

---
+++ www/functions/array/reset.php
@@ -0,0 +1,14 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('reset()');
+
+
+$array = [0,1,2,3,4,5];
+HTML::p(current($array));
+next($array);
+next($array);
+HTML::p(current($array));
+var_dump(reset($array));
+HTML::p(current($array));
+
+$array = [];
+var_dump(reset($array));
