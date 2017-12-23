657910ccb2e033424e6266a70970c3487fb6d582
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-12-23 13:13:14 -0800

    [feature] method_exists()

---
+++ www/functions/classobj/method_exists.php
@@ -0,0 +1,29 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('method_exists');
+
+class A
+{
+    public function fa()
+    {
+
+    }
+
+    protected function fb()
+    {
+
+    }
+    private function fc()
+    {
+
+    }
+
+    private static function fd()
+    {
+
+    }
+}
+
+HTML::dump(method_exists('A', 'fa')); // true
+HTML::dump(method_exists(new A, 'fb')); // true
+HTML::dump(method_exists(new A, 'fc')); // true
+HTML::dump(method_exists(new A, 'fd')); // true
--- www/map.php
+++ www/map.php
@@ -283,6 +283,7 @@ return [
             new \Doc\models\DocPage('interface_exists', 'interface_exists'),
             new \Doc\models\DocPage('is_a', 'is_a'),
             new \Doc\models\DocPage('is_subclass_of', 'is_subclass_of'),
+            new \Doc\models\DocPage('method_exists', 'method_exists'),
         ]),
     ]),
 ];
