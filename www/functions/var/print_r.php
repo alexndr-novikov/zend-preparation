e74090a6034f0f36990ef98adaf630a0ee3897bd
Aleksandr Novikov <alexndr.novikov@gmail.com>
2017-12-28 18:37:17 +0300

    [feature] print_r()

---
+++ www/functions/var/print_r.php
@@ -0,0 +1,28 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('print_r');
+
+
+class A
+{
+    public $a = 1;
+    protected $b = 2;
+    private $c = 3;
+
+    public static $d = 4;
+    protected static $e = 5;
+    private static $f = 6;
+}
+
+print_r(new A);
+HTML::br();
+print_r(1);
+HTML::br();
+print_r('a');
+HTML::br();
+print_r([]);
+HTML::br();HTML::br();
+$a = print_r([]);
+var_dump($a);
+HTML::br();
+$a = print_r([], true);
+var_dump($a);
