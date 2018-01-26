80121f6d2df15151a2d7f0c4e9f52241159266ea
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-01-26 20:32:15 +0300

    [feautre] md5_file

--- www/functions/strings/md5_file.php
+++ www/functions/strings/md5_file.php
@@ -3,6 +3,7 @@ HTML::h1('md5_file');
 
 
 HTML::dump(md5_file(__DIR__ . '/md5.php'));
+HTML::dump(md5_file(__DIR__ . '/md5.php', true));
 HTML::dump(md5_file(__DIR__ . '/md5.php') == md5_file(__DIR__ . '/md5_file.php'));
 HTML::dump(md5_file(__DIR__ . '/md5.php') == md5_file(__DIR__ . '/md5.php'));
 HTML::dump(md5_file('https://google.com'));
