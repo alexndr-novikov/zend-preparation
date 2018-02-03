3a82b29c1d8fa76f5bcbdbc4bc0acb008b4817ef
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-02-03 23:10:42 +0300

    [feature] soundex()

---
+++ www/functions/strings/soundex.php
@@ -0,0 +1,8 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('soundex');
+
+HTML::dump(soundex('see') === soundex('sea'));
+HTML::dump(soundex('see') === soundex('seal'));
+HTML::dump(soundex('переносица') === soundex('переносится'));
+HTML::dump(soundex('банан') === soundex('чемодан'));
+HTML::dump(soundex('переносица') === soundex('perenositsya'));
