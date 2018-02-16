676c0b096fbf01ef75e4d36ad136135bf1520438
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-02-16 21:22:05 +0300

    [feature] strip_tags()

---
+++ www/functions/strings/strip_tags.php
@@ -0,0 +1,26 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('strip_tags');
+
+
+$str = '
+Lorem ipsum dolor sit amet,
+<p>consectetur</p> adipiscing elit,
+sed do <strong>eiusmod</strong> tempor incididunt ut
+labore et dolore magna aliqua.
+<?php echo 1; ?>
+<?php
+//comment
+?>
+<!-- HTML COMMENT -->
+Ut enim ad minim veniam,<hr>
+quis nostrud exercitation ullamco<br/>
+laboris nisi ut aliquip<br>
+ex ea
+commodo
+consequat.';
+
+HTML::p(htmlspecialchars(strip_tags($str)));
+HTML::p(htmlspecialchars(strip_tags($str, '<br>')));
+HTML::p(htmlspecialchars(strip_tags($str, '<strong>')));
+HTML::p(htmlspecialchars(strip_tags($str, '<php>')));
+HTML::p(htmlspecialchars(strip_tags($str, '<hr>')));
