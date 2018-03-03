624ed13d939bdcb9a2299402990d0de43dfe557c
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-03-03 18:53:37 +0300

    [feature] html_entity_decode()

---
+++ www/functions/strings/html_entity_decode.php
@@ -0,0 +1,10 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('html_entity_decode');
+
+
+$string = "I'll \"walk\" to <p>the</p> <script>console.log('it\'s alive')</script>";
+
+HTML::dump($string = htmlspecialchars($string));
+HTML::dump(html_entity_decode($string));
+HTML::dump(html_entity_decode($string, ENT_NOQUOTES | ENT_HTML5, 'cp1251'));
+HTML::dump(html_entity_decode($string, ENT_NOQUOTES | ENT_HTML5, 'utf-8'));
