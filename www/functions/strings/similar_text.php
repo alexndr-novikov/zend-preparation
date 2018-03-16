2f1134e9092fbc9bf39a42b22917cdd6f0199a88
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-03-16 21:05:26 +0300

    [feature] similar_text()

---
+++ www/functions/strings/similar_text.php
@@ -0,0 +1,30 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('similar_text');
+$string1 = 'Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut
+labore et dolore magna aliqua.
+
+Ut enim ad minim veniam,
+quis nostrud exercitation ullamco
+laboris nisi ut aliquip
+ex ea
+commodo
+consequat.';
+
+$string2 = 'Ut enim ad minim veniam,
+quis nostrud exercitation ullamco
+laboris nisi ut aliquip
+ex ea
+commodo
+consequat.';
+
+$percent = 0;
+HTML::dump(similar_text($string1, $string1));
+HTML::dump(similar_text($string1, $string2));
+HTML::br();
+HTML::dump(similar_text($string1, $string2, $percent));
+HTML::dump($percent);
+HTML::br();
+HTML::dump(similar_text($string1, $string1, $percent));
+HTML::dump($percent);
