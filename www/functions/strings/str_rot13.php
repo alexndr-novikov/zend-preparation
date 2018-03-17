53c7bf12d2a94094ce79373f83cfc713d8c6aff1
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-03-17 21:13:18 +0300

    [feature] str_rot13()

---
+++ www/functions/strings/str_rot13.php
@@ -0,0 +1,10 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('str_rot13');
+
+$string = implode('', range('a', 'z'));
+$string2 = implode('', range('a', 'z')) . implode('', range(0, 9));
+HTML::dump($string);
+HTML::dump(str_rot13($string));
+HTML::dump($string2);
+HTML::dump($string2 = str_rot13($string2));
+HTML::dump(str_rot13($string2));
