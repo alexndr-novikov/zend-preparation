00601afc051401abcc9e8d889a42a3e36083afa3
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-03-20 22:15:22 +0300

    [feature] strcoll()

---
+++ www/functions/strings/strcoll.php
@@ -0,0 +1,14 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('strcoll');
+
+
+$a = 'a';
+$b = 'A';
+
+HTML::dump(strcmp ($a, $b));
+
+setlocale (LC_COLLATE, 'C');
+HTML::dump(strcoll($a, $b));
+
+setlocale (LC_COLLATE, 'de_DE');
+HTML::dump(strcoll($a, $b));
