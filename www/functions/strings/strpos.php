e17f7cf853227aff3f3f36a199c2377269252f66
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-03-30 23:10:43 +0300

    [feature] strpos()

---
+++ www/functions/strings/strpos.php
@@ -0,0 +1,8 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('strpos');
+
+$str = 'hello Hell hello';
+HTML::dump(strpos($str, 'hell'));
+HTML::dump(strpos($str, 'hell', 2));
+HTML::dump(strpos($str, 'hell', -7));
+HTML::dump(strpos($str, 'never', 2));
