6e244d0d9a5e055051a5349cfb3edbaf5b17e8ed
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-04-02 21:49:10 +0300

    [feature] strrpos()

---
+++ www/functions/strings/strrpos.php
@@ -0,0 +1,10 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('strrpos');
+
+
+$str = 'hello Hell Hello';
+HTML::dump(strrpos($str, 'hell'));
+HTML::dump(strrpos($str, 'hell', 2));
+HTML::dump(strripos($str, 'hell', 2));
+HTML::dump(strrpos($str, 'hell', -7));
+HTML::dump(strrpos($str, 'never', 2));
