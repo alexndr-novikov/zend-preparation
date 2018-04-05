e8e4f22bd5a3d2b61d8cd7a418cb0cc9f597c1d8
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-04-05 11:36:06 +0300

    [feature] strtolower()

---
+++ www/functions/strings/strtolower.php
@@ -0,0 +1,8 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('strtolower');
+
+$upperRegistryName = 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT. ACCUSAMUS ALIQUID CONSEQUUNTUR CORPORIS 
+DEBITIS INVENTORE, IPSAM MAGNAM MAIORES MINUS NUMQUAM QUIBUSDAM SINT TEMPORA TEMPORE! EOS IURE NISI SINT SOLUTA SUSCIPIT VELIT.
+ĄĄĄĄĄĄĄĄĄĄĄĄ';
+
+HTML::p(strtolower($upperRegistryName)); // Ą not converted cause does not belong to current locale
