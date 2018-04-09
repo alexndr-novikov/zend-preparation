69a838bbd178114219ee80862e7a1393fa6c82ce
Aleksandr Novikov <alexndr.novikov@gmail.com>
2018-04-09 22:43:49 +0300

    [feature] substr()

---
+++ www/functions/strings/substr.php
@@ -0,0 +1,12 @@
+<?php use \Doc\helpers\HTML;
+HTML::h1('substr');
+
+$str = 'abcdefg';
+
+HTML::dump(substr($str, 0, 2));
+HTML::dump(substr($str, 0, 700));
+HTML::dump(substr($str, 0, -2));
+HTML::dump(substr($str, -4, 2));
+HTML::dump(substr($str, -4, -1));
+HTML::dump(substr($str, 100, -1));
+HTML::dump(substr($str, 3));
